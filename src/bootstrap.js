0b4a6015f6d717718a5b1d934e5ac8f35e21f5a7
eugustavo <gustavo.ifto@gmail.com>
2020-06-09 22:17:36 -0300

    🃏 Test Linux

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
 
-const NODE_ENV = process.env.NODE_ENV.replace(/\s/g, '');
+// const NODE_ENV = process.env.NODE_ENV.replace(/\s/g, '');
 
 dotenv.config({
-  path: NODE_ENV === 'test' ? '.env.test' : '.env',
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
 });
